244dac259ef8579950367af93ccbefb698881b71
Aayush10530 <aayushmishra01530@gmail.com>
2026-01-01 02:16:29 +0530

    lock

--- backend/routes/customTour.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCustomTourRequest } = require('../controllers/customTourController');
-
-router.post('/', createCustomTourRequest);
-
-module.exports = router; 
